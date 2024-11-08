8694bc07a18f4219eb468cb50c4788a04b40fb00
Ivan Enzhaev <8observer8@gmail.com>
2024-11-09 02:14:13 +0400

    Initial commit

---
+++ config-win.bat
@@ -0,0 +1,1 @@
+cmake -G "MinGW Makefiles" -S . -B dist/win -Dbullet_DIR=E:/libs/bullet3-2.83.7-prefix/win/lib/cmake/bullet -DSDL3_DIR=E:/libs/sdl-3.1.3-prefix/win/lib/cmake/SDL3 -DCMAKE_BUILD_TYPE=Release
